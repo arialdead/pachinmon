934dc8a31a75267e6d35fe67448c708a081dadc0
arial <zeypixcontact@gmail.com>
2025-05-19 18:09:21 +0200

    caca

--- Scripts/ParallaxBackground.gd
+++ Scripts/ParallaxBackground.gd
@@ -6,15 +6,15 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var vp_size = DisplayServer.window_get_size()
-	print(vp_size)
-	var new_size = 0.1
-	if vp_size.x > vp_size.y:
-		new_size = float(vp_size.x)/float(384)
-		$Ciel.scale = Vector2(new_size,new_size)
-		print(new_size)
+	#var vp_size = DisplayServer.window_get_size()
+	#if vp_size.x > vp_size.y:
+		#var ratio = float(vp_size.x)/236.0
+		#var p = 1
+		#while p < ratio:
+			#p *= 2
+		#$Ciel.scale = Vector2(p,p)
+		#print($Ciel.scale)
 	#$ParallaxLayer.motion_offset.x += 0.3
 	#$ParallaxLayer2.motion_offset.x += 0.6
 	#$ParallaxLayer3.motion_offset.x += 0.9
