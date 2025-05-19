1c024e0f78626459ce38f1d39c36fbaf1be7def6
JuxACAB <jux70x@gmail.com>
2025-05-19 20:34:01 +0200

    Size

--- Scripts/ParallaxBackground.gd
+++ Scripts/ParallaxBackground.gd
@@ -8,13 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var vp_size = DisplayServer.window_get_size()
-	print(vp_size)
-	var new_size = 0.1
-	if vp_size.x > vp_size.y:
-		new_size = float(vp_size.x)/float(384)
-		$Ciel.scale = Vector2(new_size,new_size)
-		print(new_size)
+
 	#$ParallaxLayer.motion_offset.x += 0.3
 	#$ParallaxLayer2.motion_offset.x += 0.6
 	#$ParallaxLayer3.motion_offset.x += 0.9
